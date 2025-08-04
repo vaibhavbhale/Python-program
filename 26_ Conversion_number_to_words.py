4cad50440ddf532a40211e2cf8c0be40d137085d
vaibhavbhale <164491508+vaibhavbhale@users.noreply.github.com>
2025-08-04 18:33:07 +0530

    Add files via upload

---
+++ 26_ Conversion_number_to_words.py
@@ -0,0 +1,18 @@
+dict= {
+    '0': 'Zero',
+    '1': 'One',
+    '2': 'Two',
+    '3': 'Three',
+    '4': 'Four',
+    '5': 'Five',
+    '6': 'Six',
+    '7': 'Seven',
+    '8': 'Eight',
+    '9': 'Nine'
+}
+
+
+n = input("Enter a number: ")
+for i in n:
+    if i in dict:
+        print(dict[i],end=" ")
