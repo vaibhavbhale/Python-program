36f16f91ec31d2ece2947d67a6426e4d63e8492d
vaibhavbhale <164491508+vaibhavbhale@users.noreply.github.com>
2025-10-02 21:15:11 +0530

    Add files via upload

---
+++ 82_char_alp_or_not.py
@@ -0,0 +1,14 @@
+ch =input("Enter the characters :")
+
+if 'a' <= ch <= 'z' or 'A' <= ch <= 'Z':
+    print(f"The  character {ch} is an Alphabet")
+else:
+    print(f"The  character {ch} is not an Alphabet")
+
+'''
+ch =input("Enter the characters :")
+if 97 <= ord(ch) <= 122 or 65 <= ord(ch)<= 90:
+    print(f"The  character {ch} is an Alphabet")
+else:
+    print(f"The  character {ch} is not an Alphabet")
+'''
---
+++ 83_char_ASCII_Val.py
@@ -0,0 +1,4 @@
+ch =input("Enter the characters:")
+Ascii_val = ord(ch)
+
+print(f"The ASCII value of {ch} is {Ascii_val}")
